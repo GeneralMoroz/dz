370bf368ba9fe3f4df3f361ce9e2176b97e4e457
GeneralMoroz <wednesday@bk.ru>
2021-11-23 02:04:19 +0300

    считал

--- Alexander/src/main/java/ru/stqa/pft/Alexander/IgorProgram.java
+++ Alexander/src/main/java/ru/stqa/pft/Alexander/IgorProgram.java
@@ -4,6 +4,7 @@ public class IgorProgram {
 
   public static void main(String[] args) {
     System.out.println("thank you Alexander for the opportunity to become an IT specialist!");
-  }
+  int x = 5;
+    System.out.println( "площадь квадрата со стороной" + x + "=" +(x*x));}
 
 }

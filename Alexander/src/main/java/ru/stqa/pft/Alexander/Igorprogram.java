a4a5cc1e62c264078163d8931139b4f6dd3fc6f8
GeneralMoroz <wednesday@bk.ru>
2021-11-22 21:38:49 +0300

    создан паке для моей первой программы

--- Alexander/src/main/java/ru/stqa/pft/Alexander/Igorprogram.java
+++ Alexander/src/main/java/ru/stqa/pft/Alexander/Igorprogram.java
@@ -1,4 +1,6 @@
-public class ForAlexander {
+package ru.stqa.pft.Alexander;
+
+public class Igorprogram {
 
  public static void main(String[] args) {
 System.out.println("thank you Alexander for the opportunity to become an IT specialist!");

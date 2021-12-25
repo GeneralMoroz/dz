481a4e476b5029eeb4417fe73e6837614af613de
GeneralMoroz <94625002+GeneralMoroz@users.noreply.github.com>
2021-12-25 18:39:05 +0300

    в тесте для создания группы на сайте адресбук, добавлены новые методы, добавлен объект на основе метода по созданию групп

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook;
 
+import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
@@ -19,35 +20,74 @@ public class GroupCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://testadress.ru/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("/html/body/div/div[4]/form/input[3]")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
+    gotogrouppage();
+    //зайти на вкладку группы
+    initgroupcreation();
+    //нажать кнопку создать новую группу
+    fillgroupform(new GroupData("test11", "test22", "test33"));
+    //вписать информацию
+    submitgroupcreation();
+    //нажать добавить информацию
+    returntogrouppage();
+    //нажать на вкладку группы
+    marksecondgroup();
+    //выдедить из списка вторую группу, которая повторно создалась
+    deletegroupcreation();
+    //нажать кнопку удалить выделенную группу
+    returntogrouppage();
+    //вернуться в вкладку группы
+  }
+
+  private void deletegroupcreation() {
+    wd.findElement(By.xpath("/html/body/div/div[4]/form/input[2]")).click();
+  }
+
+  private void marksecondgroup() {
+    wd.findElement(By.xpath("/html/body/div/div[4]/form/input[7]")).click();
+  }
+
+  private void returntogrouppage() {
     wd.findElement(By.xpath("/html/body/div/div[3]/ul/li[3]/a")).click();
-    wd.findElement(By.name("new")).click();
+  }
+
+  private void submitgroupcreation() {
+    wd.findElement(By.xpath("/html/body/div/div[4]/form/input[2]")).click();
+  }
+
+  private void fillgroupform(@NotNull GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("test11");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test22");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("test33");
-    wd.findElement(By.xpath("/html/body/div/div[4]/form/input[2]")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initgroupcreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotogrouppage() {
     wd.findElement(By.xpath("/html/body/div/div[3]/ul/li[3]/a")).click();
-    wd.findElement(By.xpath("/html/body/div/div[4]/form/input[7]")).click();
-    wd.findElement(By.xpath("/html/body/div/div[4]/form/input[2]")).click();
-    wd.findElement(By.xpath("/html/body/div/div[3]/ul/li[3]/a")).click();
-    wd.findElement(By.xpath("/html/body/div/div[1]/form/a")).click();
   }
 
   @AfterMethod(alwaysRun = true)
